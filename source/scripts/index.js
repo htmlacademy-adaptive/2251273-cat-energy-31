2563ab652afe6513def590b913d40b5769ac6add
Echenko <119807032+echenko@users.noreply.github.com>
2024-04-26 21:02:19 +0300

    4. Препроцессоры и автоматизация (#6)
    * стилизация главной страницы
    
    * правит ошибки кода
    
    * начинает стилизацию каталога
    
    * разметка страницы каталога - доп товаров
    
    * 23-04-24 промежуточный на созвон
    
    * правка бэм
    
    * чистит стили
    
    * делает базовую стилизацию всех страниц
    
    * чистит код
    
    * удаляет коменты
    
    * правит маркер
    
    * исправляет замечания

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,11 @@
 /* в этот файл добавляет скрипты*/
+// решение в лоб... научусь переделаю...
+const menuList = document.querySelector('.navigation__list');
+const menuButton = document.querySelector('.page-header__button');
+
+menuList.classList.remove('navigation__list--no-js');
+menuButton.classList.remove('page-header__button--no-js');
+
+menuButton.addEventListener('click', () => {
+  menuList.classList.toggle('navigation__list--closed');
+});
