8259db352d359ddd44f148afcd4565655fffbce0
Echenko <119807032+echenko@users.noreply.github.com>
2024-05-08 19:13:54 +0300

    5. Векторная графика (#7)
    * экспортирует векторную графику
    
    * добавляет векторную графику главной страницы
    
    * правит слайдер и функционал
    
    * завершает векторную графику главной страницы
    
    * Чистка
    
    * добавляет состояния
    
    * начинает адаптив индекса
    
    * добавляет адаптив главной страницы
    
    * делает адаптив каталога
    
    * Исправляет замечания
    
    * правит логотип

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -3,9 +3,18 @@
 const menuList = document.querySelector('.navigation__list');
 const menuButton = document.querySelector('.page-header__button');
 
+const pageWidth = document.documentElement.clientWidth;
+
 menuList.classList.remove('navigation__list--no-js');
 menuButton.classList.remove('page-header__button--no-js');
 
 menuButton.addEventListener('click', () => {
   menuList.classList.toggle('navigation__list--closed');
+  menuButton.classList.toggle('page-header__button--closed');
 });
+
+// задача при изменении ширины экрана убирать класс "navigation__list--closed" решаю...
+if (pageWidth > 768) {
+  menuList.classList.add('navigation__list--no-js');
+  menuButton.classList.add('page-header__button--no-js');
+}
